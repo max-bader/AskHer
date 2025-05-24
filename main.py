a4810751b8c92c368ac1dd3f558b1511442f593b
imjamesruiz <jamesruiz610@gmail.com>
2025-05-24 02:21:48 -0700

    backend endpoints with responses/questions routing and schema

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
-from supabase_client import client  # this uses httpx + service role key
-from typing import List
+from routes.questions import router as questions_router
+from routes.responses import router as responses_router
 
 app = FastAPI()
 
-# @app.get("/")
-# def root():
-#     return {"message": "Backend is running"}
+@app.get("/")
+def root():
+    return {"message": "Backend is running"}
 
-@app.get("/questions")
-def get_questions():
-    res = client.get("/questions?select=*")
-    if res.status_code != 200:
-        return {"error": res.text}
-    return res.json()
+app = FastAPI()
+
+app.include_router(questions_router, tags=["Questions"])
+
+app.include_router(responses_router, tags=["Responses"])
+
+
+        
+
